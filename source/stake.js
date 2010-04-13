e4137fdb3c7ec11a0cb6210eeab48b4206f13f74
James Coglan <jcoglan@googlemail.com>
2010-04-13 19:37:25 +0000

    Implement the + operator by assigning minimum repetition counts to RepeatParsers.

--- source/stake.js
+++ source/stake.js
@@ -57,14 +57,22 @@ Stake.extend({
   }),
   
   RepeatParser: new JS.Class(Stake.Parser, {
-    initialize: function(parser) {
-      this._parser = parser;
+    extend: {
+      create: function(minimum, parser) {
+        return new this(parser, minimum);
+      }
+    },
+    
+    initialize: function(parser, minimum) {
+      this._parser  = parser;
+      this._minimum = minimum || 0;
     },
     
     consume: function(input, offset) {
       var elements  = [],
           textValue = '',
           counter   = offset,
+          remaining = this._minimum,
           node      = true;
       
       while (node = this._parser.consume(input, counter)) {
@@ -72,7 +80,9 @@ Stake.extend({
         input = input.substring(node.textValue.length);
         textValue += node.textValue;
         counter   += node.textValue.length;
+        remaining -= 1;
       }
+      if (remaining > 0) return null;
       return this._syntaxNode(textValue, offset, elements);
     }
   }),
--- spec/stake/repeat_parser_spec.js
+++ spec/stake/repeat_parser_spec.js
@@ -1,53 +1,109 @@
 Stake.RepeatParserSpec = JS.Test.describe(Stake.RepeatParser, function() { with(this) {
-  before(function() { with(this) {
-    this.parser = Stake.Parser.fromSexp(
-                  ['repeat', ['string', 'foo']])
-  }})
-  
-  it('matches zero occurences of the pattern', function() { with(this) {
-    assertEqual( {textValue: '', offset: 0, elements: []}, parser.parse('') )
-  }})
-  
-  it('matches one occurence of the pattern', function() { with(this) {
-    assertEqual( {
-        textValue: 'foo',
-        offset: 0,
-        elements: [
-          {textValue: 'foo', offset: 0, elements: []}
-        ]
-      },
-      parser.parse('foo') )
-  }})
-  
-  it('matches more than one occurence of the pattern', function() { with(this) {
-    assertEqual( {
-        textValue: 'foofoofoo',
-        offset: 0,
-        elements: [
-          {textValue: 'foo', offset: 0, elements: []},
-          {textValue: 'foo', offset: 3, elements: []},
-          {textValue: 'foo', offset: 6, elements: []}
-        ]
-      },
-      parser.parse('foofoofoo') )
-  }})
-  
-  it('does not match superstrings of the repeated pattern', function() { with(this) {
-    assertNull( parser.parse('foofood') )
-  }})
-  
-  describe('followed by more of the repeated pattern', function() { with(this) {
+  describe('with zero minimum occurences', function() { with(this) {
     before(function() { with(this) {
       this.parser = Stake.Parser.fromSexp(
-                    ['sequence',
-                      ['repeat', ['string', 'foo']],
-                      ['string', 'foo']])
+                    ['repeat', 0, ['string', 'foo']])
     }})
     
-    it('does not parse any number of occurences', function() { with(this) {
+    it('matches zero occurences of the pattern', function() { with(this) {
+      assertEqual( {textValue: '', offset: 0, elements: []}, parser.parse('') )
+    }})
+    
+    it('matches one occurence of the pattern', function() { with(this) {
+      assertEqual( {
+          textValue: 'foo',
+          offset: 0,
+          elements: [
+            {textValue: 'foo', offset: 0, elements: []}
+          ]
+        },
+        parser.parse('foo') )
+    }})
+    
+    it('matches more than one occurence of the pattern', function() { with(this) {
+      assertEqual( {
+          textValue: 'foofoofoo',
+          offset: 0,
+          elements: [
+            {textValue: 'foo', offset: 0, elements: []},
+            {textValue: 'foo', offset: 3, elements: []},
+            {textValue: 'foo', offset: 6, elements: []}
+          ]
+        },
+        parser.parse('foofoofoo') )
+    }})
+    
+    it('does not match superstrings of the repeated pattern', function() { with(this) {
+      assertNull( parser.parse('foofood') )
+    }})
+    
+    describe('followed by more of the repeated pattern', function() { with(this) {
+      before(function() { with(this) {
+        this.parser = Stake.Parser.fromSexp(
+                      ['sequence',
+                        ['repeat', 0, ['string', 'foo']],
+                        ['string', 'foo']])
+      }})
+      
+      it('does not parse any number of occurences', function() { with(this) {
+        assertNull( parser.parse('') )
+        assertNull( parser.parse('foo') )
+        assertNull( parser.parse('foofoo') )
+      }})
+    }})
+  }})
+  
+  describe('with one minimum occurence', function() { with(this) {
+    before(function() { with(this) {
+      this.parser = Stake.Parser.fromSexp(
+                    ['repeat', 1, ['string', 'foo']])
+    }})
+    
+    it('does not match zero occurences of the pattern', function() { with(this) {
       assertNull( parser.parse('') )
-      assertNull( parser.parse('foo') )
-      assertNull( parser.parse('foofoo') )
+    }})
+    
+    it('matches one occurence of the pattern', function() { with(this) {
+      assertEqual( {
+          textValue: 'foo',
+          offset: 0,
+          elements: [
+            {textValue: 'foo', offset: 0, elements: []}
+          ]
+        },
+        parser.parse('foo') )
+    }})
+    
+    it('matches more than one occurence of the pattern', function() { with(this) {
+      assertEqual( {
+          textValue: 'foofoofoo',
+          offset: 0,
+          elements: [
+            {textValue: 'foo', offset: 0, elements: []},
+            {textValue: 'foo', offset: 3, elements: []},
+            {textValue: 'foo', offset: 6, elements: []}
+          ]
+        },
+        parser.parse('foofoofoo') )
+    }})
+    
+    it('does not match superstrings of the repeated pattern', function() { with(this) {
+      assertNull( parser.parse('foofood') )
+    }})
+    
+    describe('followed by more of the repeated pattern', function() { with(this) {
+      before(function() { with(this) {
+        this.parser = Stake.Parser.fromSexp(
+                      ['sequence',
+                        ['repeat', 1, ['string', 'foo']],
+                        ['string', 'foo']])
+      }})
+      
+      it('does not parse any number of occurences', function() { with(this) {
+        assertNull( parser.parse('') )
+        assertNull( parser.parse('foo') )
+        assertNull( parser.parse('foofoo') )
+      }})
     }})
   }})
 }})
