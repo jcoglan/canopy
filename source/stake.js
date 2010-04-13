ac23307c3239215370a348ac34e5ff88b7c01338
James Coglan <jcoglan@googlemail.com>
2010-04-13 08:59:46 +0100

    Add a MaybeParser.

--- source/stake.js
+++ source/stake.js
@@ -45,6 +45,17 @@ Stake.extend({
 });
 
 Stake.extend({
+  MaybeParser: new JS.Class(Stake.Parser, {
+    initialize: function(parser) {
+      this._parser = parser;
+    },
+    
+    consume: function(input, offset) {
+      var node = this._parser.consume(input, offset);
+      return node || this._syntaxNode('', offset);
+    }
+  }),
+  
   SequenceParser: new JS.Class(Stake.Parser, {
     extend: {
       create: function() {
