3c5c808e80207c8250a73de578a4c0b961bdefa8
James Coglan <jcoglan@googlemail.com>
2010-04-13 23:08:09 +0100

    Helium-compatible build file.

--- spec/runner.js
+++ spec/runner.js
@@ -2,7 +2,7 @@ JSCLASS_PATH = 'vendor/js.class/build/min'
 load(JSCLASS_PATH + '/loader.js')
 
 JS.Packages(function() { with(this) {
-    file('source/stake.js').provides('Stake')
+    file('build/stake-min.js').provides('Stake')
     autoload(/^(.*)Spec$/, {from: 'spec', require: '$1'})
 }})
 
