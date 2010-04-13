cb86e8e908f41962222efd65279009178d6c89da
James Coglan <jcoglan@googlemail.com>
2010-04-13 09:01:15 +0100

    Specs in alphabetical order.

--- spec/stake_spec.js
+++ spec/stake_spec.js
@@ -1,23 +1,20 @@
 StakeSpec = JS.Test.describe(Stake, function() { with(this) {
-  describe('StringParser', function() { with(this) {
+  describe('MaybeParser', function() { with(this) {
     before(function() { with(this) {
-      this.parser = Stake.Parser.fromSexp(['string', 'foo'])
+      this.parser = Stake.Parser.fromSexp(
+                    ['maybe', ['string', 'jc']])
     }})
     
-    it('parses the string it contains', function() { with(this) {
-      assertEqual( {textValue: 'foo', offset: 0, elements: []}, parser.parse('foo') )
+    it('parses if its pattern is present', function() { with(this) {
+      assertEqual( {textValue: 'jc', offset: 0, elements: []}, parser.parse('jc') )
     }})
     
-    it('does not parse other strings', function() { with(this) {
-      assertNull( parser.parse('bar') )
+    it('parses if no input is given', function() { with(this) {
+      assertEqual( {textValue: '', offset: 0, elements: []}, parser.parse('') )
     }})
     
-    it('does not parse superstrings of itself', function() { with(this) {
-      assertNull( parser.parse('food') )
-    }})
-    
-    it('does not parse the empty string', function() { with(this) {
-      assertNull( parser.parse('') )
+    it('does not parse if different input is given', function() { with(this) {
+      assertNull( parser.parse('gc') )
     }})
   }})
   
@@ -55,22 +52,25 @@ StakeSpec = JS.Test.describe(Stake, function() { with(this) {
     }})
   }})
   
-  describe('MaybeParser', function() { with(this) {
+  describe('StringParser', function() { with(this) {
     before(function() { with(this) {
-      this.parser = Stake.Parser.fromSexp(
-                    ['maybe', ['string', 'jc']])
+      this.parser = Stake.Parser.fromSexp(['string', 'foo'])
     }})
     
-    it('parses if its pattern is present', function() { with(this) {
-      assertEqual( {textValue: 'jc', offset: 0, elements: []}, parser.parse('jc') )
+    it('parses the string it contains', function() { with(this) {
+      assertEqual( {textValue: 'foo', offset: 0, elements: []}, parser.parse('foo') )
     }})
     
-    it('parses if no input is given', function() { with(this) {
-      assertEqual( {textValue: '', offset: 0, elements: []}, parser.parse('') )
+    it('does not parse other strings', function() { with(this) {
+      assertNull( parser.parse('bar') )
     }})
     
-    it('does not parse if different input is given', function() { with(this) {
-      assertNull( parser.parse('gc') )
+    it('does not parse superstrings of itself', function() { with(this) {
+      assertNull( parser.parse('food') )
+    }})
+    
+    it('does not parse the empty string', function() { with(this) {
+      assertNull( parser.parse('') )
     }})
   }})
 }})
