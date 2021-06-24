87edb5d5cb38f600c2b644e2b1df7541d0217420
James Coglan <jcoglan@gmail.com>
2021-06-24 14:10:07 +0100

    Correct the imports in spec/runner.js

--- spec/runner.js
+++ spec/runner.js
@@ -4,10 +4,11 @@ var jstest = require('jstest').Test
 
 require('./canopy/meta_grammar_spec')
 require('./canopy_spec')
+require('./canopy/compiler/action_spec')
 require('./canopy/compiler/any_char_spec')
 require('./canopy/compiler/char_class_spec')
-require('./canopy/compiler/choice_part_spec')
 require('./canopy/compiler/choice_spec')
+require('./canopy/compiler/extension_spec')
 require('./canopy/compiler/predicate_spec')
 require('./canopy/compiler/reference_spec')
 require('./canopy/compiler/repeat_spec')
