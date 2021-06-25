ac908f20465221bf242af121d875629ff6e80259
James Coglan <jcoglan@gmail.com>
2021-06-25 15:09:57 +0100

    Split some helper functions out in the Java tests

--- test/java/TerminalsTest.java
+++ test/java/TerminalsTest.java
@@ -1,3 +1,5 @@
+package canopy;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
@@ -5,17 +7,34 @@ import test.grammars.terminals.ParseError;
 import test.grammars.terminals.Terminals;
 import test.grammars.terminals.TreeNode;
 
-class AnyCharTest {
-    TreeNode node;
-
+class AnyCharTest extends ParseHelper {
     @Test
     void parsesAnySingleCharacter() throws ParseError {
-        node = Terminals.parse("any: a").elements.get(1);
-        assertEquals("a", node.text);
-        assertEquals(5, node.offset);
+        expectParse(Terminals.parse("any: a")).text("a").offset(5);
+        expectParse(Terminals.parse("any: !")).text("!").offset(5);
+    }
+}
 
-        node = Terminals.parse("any: !").elements.get(1);
-        assertEquals("!", node.text);
-        assertEquals(5, node.offset);
+class ParseHelper {
+    NodeMatcher expectParse(TreeNode node) {
+        return new NodeMatcher(node.elements.get(1));
+    }
+}
+
+class NodeMatcher {
+    private TreeNode node;
+
+    NodeMatcher(TreeNode node) {
+        this.node = node;
+    }
+
+    NodeMatcher text(String text) {
+        assertEquals(text, node.text);
+        return this;
+    }
+
+    NodeMatcher offset(int offset) {
+        assertEquals(offset, node.offset);
+        return this;
     }
 }
